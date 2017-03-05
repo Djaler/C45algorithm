2dd623353f26db0cd4834335836ed9e1dd0d6565
Kirill Romanov <djaler1@gmail.com>
2017-03-05 20:12:46 +0300

    Some refactoring

--- c45.py
+++ c45.py
@@ -1,37 +1,19 @@
 import math
+
 import utils
 
 
-def freq(table, col, v):
-    """ Returns counts of variant _v_
-        in column _col_ of table _table_.
-    """
-    return table[col].count(v)
-
-
 def info(table, res_col):
-    """ Calculates the entropy of the table _table_
-        where res_col column = _res_col_.
-    """
-    s = 0 # sum
-    for v in utils.deldup(table[res_col]):
-        p = freq(table, res_col, v) / float(len(table[res_col]))
-        s += p * math.log(p, 2)
-    return -s
+    freqs = []
+    for variant in set(table[res_col]):
+        freqs.append(table[res_col].count(variant) / len(table[res_col]))
+    return -sum(freq * math.log(freq, 2) for freq in freqs)
 
 
-def infox(table, col, res_col):
-    """ Calculates the entropy of the table _table_
-        after dividing it on the subtables by column _col_.
-    """
-    s = 0 # sum
-    for subt in utils.get_subtables(table, col):
-        s += (float(len(subt[col])) / len(table[col])) * info(subt, res_col)
-    return s
-    
+def info_x(table, col, res_col):
+    return sum(len(subtable[col]) / len(table[col]) * info(subtable, res_col)
+               for subtable in utils.get_subtables(table, col))
 
 
-def gain(table, x, res_col):
-    """ The criterion for selecting attributes for splitting.
-    """
-    return info(table, res_col) - infox(table, x, res_col)
+def gain(table, col, res_col):
+    return info(table, res_col) - info_x(table, col, res_col)
--- utils.py
+++ utils.py
@@ -1,82 +1,27 @@
-from collections import OrderedDict
+def is_all_equals(items):
+    return all(items[0] == item for item in items)
 
 
-def deldup(li):
-    """ Deletes duplicates from list _li_
-        and return new list with unique values.
-    """
-    return list(OrderedDict.fromkeys(li))
-
-
-def is_mono(t):
-    """ Returns True if all values of _t_ are equal
-        and False otherwise.
-    """
-    for i in t:
-        if i != t[0]:
-            return False
-    return True
-
-
-def get_indexes(table, col, v):
-    """ Returns indexes of values _v_ in column _col_
-        of _table_.
-    """
-    li = []
-    start = 0
-    for row in table[col]:
-        if row == v:
-            index = table[col].index(row, start)
-            li.append(index)
-            start = index + 1
-    return li
-
-
-def get_values(t, col, indexes):
-    """ Returns values of _indexes_ in column _col_
-        of the table _t_.
-    """
-    return [t[col][i] for i in range(len(t[col])) if i in indexes]
-
-
-def del_values(t, ind):
-    """ Creates the new table with values of _ind_.
-    """
-    return {k: [v[i] for i in range(len(v)) if i in ind] for k, v in t.items()}
-
-
-def print_list_tree(tree, tab=''):
-    """ Prints list of nested lists in
-        hierarchical form.
-    """
-    print('%s[' % tab)
-    for node in tree:
-        if isinstance(node, basestring):
-            print('%s  %s' % (tab, node))
-        else:
-            print_list_tree(node, tab + '  ')
-    print('%s]' % tab)
+def create_subtable(table, column, variant):
+    indices = [index for index, item in enumerate(table[column]) if
+               item == variant]
+    
+    return {
+        column: [item for index, item in enumerate(items) if index in indices]
+        for column, items in table.items()}
 
 
 def formalize_rules(list_rules):
-    """ Gives an list of rules where
-        facts are separeted by coma.
-        Returns string with rules in
-        convinient form (such as
-        'If' and 'Then' words, etc.).
-    """
-    text = ''
-    for r in list_rules:
-        t = [i for i in r.split(',') if i]
-        text += 'If %s,\n' % t[0]
-        for i in t[1:-1]:
-            text += '   %s,\n' % i
-        text += 'Then: %s.\n' % t[-1]
-    return text
+    text = []
+    for rule in list_rules:
+        rule = [part for part in rule.split(',') if part]
+        
+        text.append('Если {},'.format(rule[0]))
+        text.extend(['\t{},'.format(part) for part in rule[1:-1]])
+        text.append('То: {}.\n'.format(rule[-1]))
+    return '\n'.join(text)
 
 
-def get_subtables(t, col):
-    """ Returns subtables of the table _t_
-        divided by values of the column _col_.
-    """
-    return [del_values(t, get_indexes(t, col, v)) for v in deldup(t[col])]
+def get_subtables(table, divide_column):
+    return [create_subtable(table, divide_column, variant) for variant in
+            set(table[divide_column])]
